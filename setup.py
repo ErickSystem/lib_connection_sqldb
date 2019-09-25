07589b05afe3b4f027dd430ed8bee3d8e7fba252
Ericles Henrique <ericles.lara@mutantbr.com>
2019-09-25 19:23:17 -0300

    upgrade version SQLAlchemy, to resolve vulnerable version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lib_connection_sqldb',
-    version='1.0.2',
+    version='1.1.0',
     description='relational database libraries',
     url='',
     author='Erick Henrique',
@@ -14,7 +14,7 @@ setup(
     license='Public',
     install_requires = [
         'lib_formatter_logger~=1.0',
-        'SQLAlchemy~=1.2.6',
+        'SQLAlchemy~=1.3.0',
         'pymysql~=0.8.0'
     ],
     packages=find_packages(),
